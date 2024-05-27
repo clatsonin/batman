cd2ce8e217b623d2e89ab2714506dbfdb4188d3d
Clatson <124655279+clatsonin@users.noreply.github.com>
2024-05-27 15:01:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,113 @@
+# The Batman 🦇🔥
+
+Here Batman is a Disaster Aid Bot is a web application designed to provide timely and efficient assistance during crises and disaster situations. Leveraging advanced AI technology, this bot offers crucial support to users by analyzing images and providing location-specific guidance, ensuring quick access to essential resources and information when it's needed most.
+
+# Key Features ⚡
+Rapid Response: The Batman bot delivers swift assistance by utilizing Google Generative AI to analyze uploaded images and generate relevant insights and responses in real-time.
+
+Location Awareness: Users can input their current location to receive personalized, location-specific guidance, including emergency contacts, evacuation routes, and safety protocols.
+
+Precautionary Measures: In addition to immediate aid, the bot offers proactive precautionary measures to help users mitigate risks and stay safe during emergencies.
+
+Community Support: The Batman bot fosters a sense of community support by providing valuable assistance and resources to users affected by disasters, promoting resilience and collective well-being.
+
+# How it works?💫
+1.Image Upload: Users upload images related to their current situation or surroundings.
+
+2.Location Input: Users provide their current location to receive personalized assistance tailored to their geographical context.
+
+3.AI Analysis: The bot utilizes powerful GEN AI- GOOGLE GEMINI algorithms to analyze images and extract relevant information, generating actionable insights and guidance.
+
+4.Instant Assistance: Within moments, users receive comprehensive responses, including emergency contacts, safety measures, and recommended actions.
+
+# Why Batman 🦇🔥
+
+Time Efficiency: The Batman bot prioritizes time-sensitive assistance, delivering rapid responses and support to users facing urgent situations.
+
+Accessibility: With a user-friendly interface and streamlined functionality, the bot ensures accessibility for individuals in distress, enabling them to access critical information and aid effortlessly.
+
+Empowerment: By providing timely and relevant assistance, The Batman bot empowers users to make informed decisions and take necessary actions to safeguard themselves and their communities during disasters.
+
+
+
+
+
+
+## Installation 🌟
+
+
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/rahulgoat/neodata-submission
+cd neodata-submission
+```
+### 2.Set-up a virtual environment:
+
+```bash
+python3 -m venv env
+```
+### 3.Activate the Virtual Environment:
+
+On macOS and Linux:
+```bash
+python3 -m venv env
+```
+On Windows:
+```bash
+.\env\Scripts\acti
+```
+### 4.Activate the Virtual Environment:
+
+Install Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+### OR
+
+
+```bash
+pip3 install -r requirements.txt
+```
+
+
+
+### 5.Set Up Environment Variables:
+
+### Create a .env file in the root directory of the project and add your Google API key:
+
+```bash
+GOOGLE_API_KEY="your_google_api_key_here"
+```
+
+### 6.Run the Streamlit App:
+
+```bash
+streamlit run app.py
+```
+### OR 
+
+### Copy the path of "app.py"
+
+```bash
+streamlit run <paste the path>
+```
+
+## Contributors
+
+•⁠  ⁠[Rahul Shanmugam S](https://github.com/rahulgoat)
+•⁠  ⁠[Clatson J](https://github.com/clatsonin)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,144 @@
+import requests
+import os
+import google.generativeai as genai
+from PIL import Image
+import io
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()  # Take environment variables from .env
+
+# Ensure the API key is set
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to load OpenAI model and get responses
+def get_gemini_response(input_text, image):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    if input_text:
+        response = model.generate_content([input_text, image])
+    else:
+        response = model.generate_content(image)
+    return response.text
+
+# Function to prompt user to input location manually
+def get_location_input():
+    return st.text_input("Enter your current location: ", key="location_input", max_chars=100, )
+
+
+
+# Create the model
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+]
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    safety_settings=safety_settings,
+    generation_config=generation_config,
+)
+
+
+chat_session = model.start_chat(history=[])
+
+# Initialize our Streamlit app
+st.set_page_config(page_title="The Batman🦇🔥")
+
+st.header("The Batman🦇🔥")
+
+# Create two columns with centered alignment
+col1, col2 = st.columns([1, 1])
+
+# File uploader in the first column
+with col1:
+    st.write("")  # Adding an empty space for vertical alignment
+    uploaded_file = st.file_uploader(
+        "Upload image here ⚡!", type=["jpg", "jpeg", "png"], key="image_uploader", help="Click here to upload an image."
+    )
+    image = None
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+# Location input in the first column
+with col1:
+    st.write("")  # Adding an empty space for vertical alignment
+    location = get_location_input()
+
+# If ask button is clicked and location is given
+if location and col1.button("Generate ⚡"):
+    if image:
+        response = get_gemini_response("Tell me about this pic", image)
+        extended_input = response + \
+            " I'm here in {location} which is a disaster. Tell me ways to escape."
+        response2 = get_gemini_response(extended_input, image)
+
+        # Response in the second column
+        with col2:
+            st.subheader("You're scenario:")
+            st.text_area("Response", response, height=200, max_chars=500)
+
+            st.subheader("Steps for precaution 💡:")
+            st.text_area("Precautionary Steps", response2,
+                         height=200, max_chars=500)
+    else:
+        st.error("Please upload an image before submitting.")
+
+# Fetch helpline numbers based on location if provided
+if location:
+    url = "https://google-search74.p.rapidapi.com/"
+    querystring = {
+        "query": f"{location} helpline number",
+        "limit": "5",
+        "related_keywords": "true"
+    }
+
+    headers = {
+        "X-RapidAPI-Key": "6786ff7868msh1bd6504ddfdda82p1331eejsndd79c4275b12",
+        "X-RapidAPI-Host": "google-search74.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    # Extract JSON response
+    data = response.json()
+
+    # Extract descriptions
+    descriptions = [result['description'] for result in data['results']]
+
+    # Optionally store the descriptions in a variable for further use
+    descriptions_text = "\n".join(descriptions)
+
+    # Send the descriptions along with the location to the model
+    response = chat_session.send_message(
+        f"{location} helpline number: " + descriptions_text
+    )
+
+    # Display the response from the model
+    st.text_area("Generated Response", response.text,
+                 height=200, max_chars=500)
+
+
+
+
